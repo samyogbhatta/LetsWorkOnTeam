cbaf824f68e5d8608895bc94ce66f72cdd119085
Samyog Bhatta <bhattasamyog2006@gmail.com>
2024-10-29 21:18:02 +0545

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,27 @@
-print("game")
+import pygame
+
+# Initialize pygame
+x = pygame.init()
+
+# Create game window
+gameWindow = pygame.display.set_mode((1200, 500))
+pygame.display.set_caption("Eat Bamboo Eat")
+
+# Game-specific variables
+exit_game = False
+game_over = False
+
+# Game loop
+while not exit_game:
+    for event in pygame.event.get():
+        print(event)
+
+        if event.type == pygame.QUIT:
+            exit_game = True
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                print("You have pressed the right arrow key")
+
+pygame.quit()
+quit()
